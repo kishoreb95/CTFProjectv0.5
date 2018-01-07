d9e4f5149e9396a63400141fbee54cb1c44da8f3
Matthew Stewart <mstewa09@calpoly.edu>
2018-01-07 10:43:24 -0800

    Press 1 to place a wall

--- Source/CTFProject/CTFProjectCharacter.cpp
+++ Source/CTFProject/CTFProjectCharacter.cpp
@@ -8,43 +8,53 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
 
 //////////////////////////////////////////////////////////////////////////
 // ACTFProjectCharacter
 
 ACTFProjectCharacter::ACTFProjectCharacter()
 {
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+   // Set size for collision capsule
+   GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
+   // set our turn rates for input
+   BaseTurnRate = 45.f;
+   BaseLookUpRate = 45.f;
 
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
+   // Don't rotate when the controller rotates. Let that just affect the camera.
+   bUseControllerRotationPitch = false;
+   bUseControllerRotationYaw = false;
+   bUseControllerRotationRoll = false;
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->JumpZVelocity = 600.f;
-	GetCharacterMovement()->AirControl = 0.2f;
+   // Configure character movement
+   GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...   
+   GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+   GetCharacterMovement()->JumpZVelocity = 600.f;
+   GetCharacterMovement()->AirControl = 0.2f;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+   // Create a camera boom (pulls in towards the player if there is a collision)
+   CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+   CameraBoom->SetupAttachment(RootComponent);
+   CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character   
+   CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+   // Create a follow camera
+   FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+   FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+   FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+   ConstructorHelpers::FObjectFinder<APlaceable> WallAsset(TEXT("Blueprint'/Game/ThirdPersonCPP/Blueprints/Placeable/Wall.Wall'"));
+   Wall = WallAsset.Object;
+
+
+	ConstructorHelpers::FObjectFinder<UBlueprint> WallAsseter(TEXT("Blueprint'/Game/ThirdPersonCPP/Blueprints/Placeable/Wall.Wall'"));
+   Waller = (UClass*)WallAsseter.Object->GeneratedClass;
+
+   Reach = 1000.f;
+
+   // Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+   // are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -52,83 +62,111 @@ ACTFProjectCharacter::ACTFProjectCharacter()
 
 void ACTFProjectCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
-	// Set up gameplay key bindings
-	check(PlayerInputComponent);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+   // Set up gameplay key bindings
+   check(PlayerInputComponent);
+   PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+   PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &ACTFProjectCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ACTFProjectCharacter::MoveRight);
+   PlayerInputComponent->BindAxis("MoveForward", this, &ACTFProjectCharacter::MoveForward);
+   PlayerInputComponent->BindAxis("MoveRight", this, &ACTFProjectCharacter::MoveRight);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &ACTFProjectCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &ACTFProjectCharacter::LookUpAtRate);
+   // We have 2 versions of the rotation bindings to handle different kinds of devices differently
+   // "turn" handles devices that provide an absolute delta, such as a mouse.
+   // "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+   PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+   PlayerInputComponent->BindAxis("TurnRate", this, &ACTFProjectCharacter::TurnAtRate);
+   PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+   PlayerInputComponent->BindAxis("LookUpRate", this, &ACTFProjectCharacter::LookUpAtRate);
 
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &ACTFProjectCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &ACTFProjectCharacter::TouchStopped);
+   // handle touch devices
+   PlayerInputComponent->BindTouch(IE_Pressed, this, &ACTFProjectCharacter::TouchStarted);
+   PlayerInputComponent->BindTouch(IE_Released, this, &ACTFProjectCharacter::TouchStopped);
 
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ACTFProjectCharacter::OnResetVR);
+   // VR headset functionality
+   PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ACTFProjectCharacter::OnResetVR);
+
+
+   PlayerInputComponent->BindAction("PlaceWall", IE_Pressed, this, &ACTFProjectCharacter::PlaceWall);
 }
 
+void ACTFProjectCharacter::PlaceWall()
+{
+   FHitResult LinetraceHit;
+   FVector StartTrace = FollowCamera->GetComponentLocation();
+   FVector EndTrace = (FollowCamera->GetForwardVector() * Reach) + StartTrace;
+
+   FCollisionQueryParams CQP;
+   CQP.AddIgnoredActor(this);
+
+   GetWorld()->LineTraceSingleByChannel(LinetraceHit, StartTrace, EndTrace, ECollisionChannel::ECC_WorldDynamic, CQP);
+
+   AActor* PotentialObject = Cast<AActor>(LinetraceHit.GetActor());
+
+   FVector DropLocation = EndTrace;
+
+   if (PotentialObject != NULL) {
+   	DropLocation = LinetraceHit.ImpactPoint + 30.0f;
+      GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, *PotentialObject->GetName());
+   }
+   FActorSpawnParameters SpawnInfo;
+   APlaceable* MyPlaceable = GetWorld()->SpawnActor<APlaceable>(Waller, DropLocation, FRotationMatrix::MakeFromY(FollowCamera->GetForwardVector()).Rotator(), SpawnInfo);
+   //Wall->SetActorLocationAndRotation(DropLocation, FRotator::ZeroRotator);
+   
+   
+}
 
 void ACTFProjectCharacter::OnResetVR()
 {
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+   UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 }
 
 void ACTFProjectCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		Jump();
+      Jump();
 }
 
 void ACTFProjectCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		StopJumping();
+      StopJumping();
 }
 
 void ACTFProjectCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+   // calculate delta for this frame from the rate information
+   AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
 
 void ACTFProjectCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+   // calculate delta for this frame from the rate information
+   AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
 void ACTFProjectCharacter::MoveForward(float Value)
 {
-	if ((Controller != NULL) && (Value != 0.0f))
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+   if ((Controller != NULL) && (Value != 0.0f))
+   {
+      // find out which way is forward
+      const FRotator Rotation = Controller->GetControlRotation();
+      const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
+      // get forward vector
+      const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+      AddMovementInput(Direction, Value);
+   }
 }
 
 void ACTFProjectCharacter::MoveRight(float Value)
 {
-	if ( (Controller != NULL) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
-	}
+   if ( (Controller != NULL) && (Value != 0.0f) )
+   {
+      // find out which way is right
+      const FRotator Rotation = Controller->GetControlRotation();
+      const FRotator YawRotation(0, Rotation.Yaw, 0);
+   
+      // get right vector 
+      const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+      // add movement in that direction
+      AddMovementInput(Direction, Value);
+   }
 }
